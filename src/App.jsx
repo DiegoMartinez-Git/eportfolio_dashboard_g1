d4fc1a842276a64c6f12d5e25da776986b518cc0
Diego <2863238@alu.murciaeduca.es>
2025-11-19 12:42:00 +0100

    prueba2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Diego Martinez hola</h1>
+      <h1>Diego Martinez holfsfsdfa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
